6b0c36b74baba68b0f1b644e4cc69bbab6e0ca2d
Albert Lionelle <albert.lionelle@colostate.edu>
2024-04-15 15:36:02 -0600

    feat: added companion and canidae

--- Module03/Companion.java
+++ Module03/Companion.java
@@ -1,3 +1,16 @@
-public class Companion {
+public abstract class Companion extends Animal {
+    private String friendlyName;
 
+    public Companion(String name) {
+        setFriendlyName(name);
+
+    }
+
+    public void setFriendlyName(String name) {
+        this.friendlyName = name;
+    }
+
+    public String getFriendlyName() {
+        return friendlyName;
+    }
 }
