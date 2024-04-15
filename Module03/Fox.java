6228e826b7cf870de3a54269dd8ce974895d3c66
Albert Lionelle <albert.lionelle@colostate.edu>
2024-04-15 15:36:16 -0600

    added fox info

--- Module03/Fox.java
+++ Module03/Fox.java
@@ -1,3 +1,42 @@
-public class Fox {
+public class Fox extends Canidae {
+
+    private static final String GENUS = "Vulpes";
+    private static final String SPECIES = "vulpes";
+
+    private static final int MAX_TRICKS_DEFAULT = 0; // foxes can do them, but don't want to do
+                                                     // them.
+
+    private final int maxTricks;
+
+    public Fox(String name) {
+        this(name, MAX_TRICKS_DEFAULT);
+    }
+
+    public Fox(String name, int maxTricks) {
+        super(name);
+        this.maxTricks = maxTricks;
+    }
+
+
+    @Override
+    public String getGenus() {
+        return GENUS;
+    }
+
+    @Override
+    public int getMaxTricks() {
+        return maxTricks;
+    }
+
+    @Override
+    public String getSound() {
+        return "What does the fox say?";
+    }
+
+    @Override
+    public String getSpecies() {
+        return SPECIES;
+    }
+
 
 }
