9e22cadcd5d8c9c15ac0e25a5927e2899db4efce
Albert Lionelle <albert.lionelle@colostate.edu>
2024-04-15 15:36:08 -0600

    added dog info

--- Module03/Dog.java
+++ Module03/Dog.java
@@ -1,3 +1,41 @@
-public class Dog {
+public class Dog extends Canidae {
+    private static final String GENUS = "Canis";
+    private static final String SPECIES = "Canis lupus";
+
+    private static final int MAX_TRICKS_DEFAULT = 5;
+
+    private final int maxTricks;
+
+    public Dog(String name) {
+        this(name, MAX_TRICKS_DEFAULT);
+    }
+
+    public Dog(String name, int maxTricks) {
+        super(name);
+        this.maxTricks = maxTricks;
+    }
+
+
+    @Override
+    public String getGenus() {
+        return GENUS;
+    }
+
+    @Override
+    public int getMaxTricks() {
+        return maxTricks;
+    }
+
+    @Override
+    public String getSound() {
+        return "barks";
+    }
+
+    @Override
+    public String getSpecies() {
+        return SPECIES;
+    }
+
+
 
 }
