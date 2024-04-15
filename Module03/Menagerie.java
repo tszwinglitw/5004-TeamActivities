4f425a8d8c561d06ac5a244ead41cafed6391dc4
Albert Lionelle <albert.lionelle@colostate.edu>
2024-04-15 15:36:25 -0600

    feat: base class added

--- Module03/Menagerie.java
+++ Module03/Menagerie.java
@@ -8,10 +8,32 @@ public class Menagerie {
 
     List<Animal> animals = new ArrayList<>();
 
+    public void addAnimal(Animal animal) {
+        animals.add(animal);
+    }
+
+
+    public int getCount() {
+        return animals.size(); // returns the total animals in the list
+    }
+
+    // adds some initial animals to the menagerie
+    private static void addTestSet(Menagerie menagerie) {
+        menagerie.addAnimal(new Dog("Fido"));
+
+
+    }
 
 
     public static void main(String[] args) {
         Menagerie fun_times = new Menagerie();
+
+
+        System.out.printf("Total in the menagerie: %d\n", fun_times.getCount()); // btw printf is
+                                                                                 // the same as in
+                                                                                 // C, it allows the
+                                                                                 // C standard
+                                                                                 // formatting.
     }
 
 }
