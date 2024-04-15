f6304a45662b71203be37debfe5d4e65dac9c6bc
Albert Lionelle <albert.lionelle@colostate.edu>
2024-04-15 12:41:58 -0600

    added driver

---
+++ Menagerie.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * Driver program for the menagerie example.
+ */
+public class Menagerie {
+
+    List<Animal> animals = new ArrayList<>();
+
+
+
+    public static void main(String[] args) {
+        Menagerie fun_times = new Menagerie();
+    }
+
+}
