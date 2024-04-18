f01fa9b54bd9156067b69dff76cdf42dea26ed7e
Albert Lionelle <albert.lionelle@colostate.edu>
2024-04-17 20:37:28 -0600

    feat: added constructor for Trick

--- Module03/Trick.java
+++ Module03/Trick.java
@@ -1,3 +1,12 @@
 public class Trick {
 
+    private String trickName;
+
+    public Trick(String trickName) {
+        this.trickName = trickName;
+    }
+
+    public String getName() {
+        return trickName;
+    }
 }
