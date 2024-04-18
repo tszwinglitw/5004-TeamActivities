e56c576f6dc37687b49bf35a808595eb177d476e
Albert Lionelle <albert.lionelle@colostate.edu>
2024-04-17 20:37:12 -0600

    feat: add test sample

--- Module03/Menagerie.java
+++ Module03/Menagerie.java
@@ -6,12 +6,16 @@ import java.util.ArrayList;
  */
 public class Menagerie {
 
-    List<Animal> animals = new ArrayList<>();
+    List<Animal> animals = new ArrayList<>(); // this is similar to a python list - will cover more
+                                              // in module 04
 
     public void addAnimal(Animal animal) {
         animals.add(animal);
     }
 
+    public List<Animal> getAnimals() {
+        return animals;
+    }
 
     public int getCount() {
         return animals.size(); // returns the total animals in the list
@@ -20,13 +24,21 @@ public class Menagerie {
     // adds some initial animals to the menagerie
     private static void addTestSet(Menagerie menagerie) {
         menagerie.addAnimal(new Dog("Fido"));
-
+        menagerie.addAnimal(new Dog("Roy", 23));
+        menagerie.addAnimal(new Cat("Cattivo", 5));
+        menagerie.addAnimal(new Fox("Robin"));
+        menagerie.addAnimal(new Cat("Gus"));
 
     }
 
 
     public static void main(String[] args) {
         Menagerie fun_times = new Menagerie();
+        addTestSet(fun_times); // static add of some test elements
+
+        for (Animal animal : fun_times.getAnimals()) {
+            System.out.println(animal);
+        }
 
 
         System.out.printf("Total in the menagerie: %d\n", fun_times.getCount()); // btw printf is
