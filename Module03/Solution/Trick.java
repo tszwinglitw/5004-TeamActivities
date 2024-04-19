ec8cb26932c45c8826198a5be5652ac6917186f1
Albert Lionelle <albert.lionelle@colostate.edu>
2024-04-18 21:22:39 -0600

    feat: added trick

--- Module03/Solution/Trick.java
+++ Module03/Solution/Trick.java
@@ -1,3 +1,20 @@
 public class Trick {
+    private String name;
 
+    public Trick(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Trick{" + "name='" + name + '\'' + '}';
+    }
 }
