27a3c9dc37c240a7e026c340c18008814680913a
Albert Lionelle <albert.lionelle@colostate.edu>
2024-04-18 18:01:15 -0600

    feat: added get for basic properties

--- Module03/Solution/Taxonomy.java
+++ Module03/Solution/Taxonomy.java
@@ -1,3 +1,42 @@
 public class Taxonomy {
+    private String commonName;
+    private String phylum;
+    private String order;
+    private String family;
+    private String genus;
+    private String species;
+    private String taxonomyClass;
+    private String kingdom;
 
+    public String getCommonName() {
+        return commonName;
+    }
+
+    public String getPhylum() {
+        return phylum;
+    }
+
+    public String getOrder() {
+        return order;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public String getGenus() {
+        return genus;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public String getTaxonomyClass() {
+        return taxonomyClass;
+    }
+
+    public String getKingdom() {
+        return kingdom;
+    }
 }
