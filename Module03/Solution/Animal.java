1ea3c93480b228370a81dbbcea6244e006c1bca2
Albert Lionelle <albert.lionelle@colostate.edu>
2024-04-18 21:05:18 -0600

    feat: added animal skeleton

--- Module03/Solution/Animal.java
+++ Module03/Solution/Animal.java
@@ -1,3 +1,26 @@
 public class Animal {
+    private Taxonomy taxonomy;
+    private String sound;
 
+    public Animal(Taxonomy taxonomy, String sound) {
+        this.taxonomy = taxonomy;
+        this.sound = sound;
+    }
+
+    public String getSound() {
+        return sound;
+    }
+
+    public void setSound(String sound) {
+        this.sound = sound;
+    }
+
+    public Taxonomy getTaxonomy() {
+        return taxonomy;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" + taxonomy.commonName() + "make " + sound + '}';
+    }
 }
