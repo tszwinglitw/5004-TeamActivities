43a2c58d941ebe8bff9df47f247acef80c0608dd
Albert Lionelle <albert.lionelle@colostate.edu>
2024-05-06 12:24:21 -0600

    feat: added template

---
+++ Module05/Person.java
@@ -0,0 +1,12 @@
+
+class Person {
+    private String name;
+
+    public Person(String theName) {
+        this.name = theName;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
---
+++ Module05/solution/Person.java
@@ -0,0 +1,13 @@
+package solution;
+
+class Person {
+    private String name;
+
+    public Person(String theName) {
+        this.name = theName;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
