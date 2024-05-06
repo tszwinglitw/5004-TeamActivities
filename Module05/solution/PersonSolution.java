2afd574f9b80d4d33de4c5eab0abd57921506c5f
Albert Lionelle <albert.lionelle@colostate.edu>
2024-05-06 12:48:40 -0600

    feat: added equals to solution using reflection

---
+++ Module05/solution/PersonSolution.java
@@ -0,0 +1,13 @@
+package solution;
+
+class PersonSolution {
+    private String name;
+
+    public PersonSolution(String theName) {
+        this.name = theName;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
---
+++ Module05/solution/StudentSolution.java
@@ -0,0 +1,61 @@
+package solution;
+
+import java.lang.reflect.Method;
+
+public class StudentSolution extends PersonSolution {
+    private int id;
+
+    public StudentSolution(String theName, int id) {
+        super(theName);
+        this.id = id;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    // protected hides this method from the public API, and only
+    // part of classes that purposely extend StudentSolution can use it
+    // which then assumes the public equals method is the one to use
+    protected boolean equalsStudent(StudentSolution other) {
+        return this.id == other.id && this.getName().equals(other.getName());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+
+        try {
+            Method[] methods = obj.getClass().getMethods();
+            for (Method method : methods) {
+                System.out.println(method.getName());
+            }
+            Method method =
+                    obj.getClass().getDeclaredMethod("equalsStudent", StudentSolution.class);
+            return equalsStudent((StudentSolution) obj);
+
+        } catch (NoSuchMethodException e) {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        int value = 10;
+        PersonSolution e1 = new StudentSolution("Alice", 1);
+        PersonSolution e2 = new StudentSolution("Bob", 2);
+        PersonSolution e3 = new StudentSolution("Carol", 3);
+        PersonSolution e5 = new PersonSolution("Alice");
+        PersonSolution arrayOfPeople[] = {e1, e2, e3};
+
+        PersonSolution e4 = new StudentSolution("Alice", 1); // same as e1?
+
+        System.out.println(e1 == arrayOfPeople[0]); // what is printed here?
+        System.out.println(e1 == e4); // what is printed here?
+
+
+        System.out.println(e1.equals(e4)); // and here?
+        System.out.println(e1.equals(e5)); // and here?
+    }
+}
