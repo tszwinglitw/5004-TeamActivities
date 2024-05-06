9aeff7288b0aec1e8bf4278e9b0297bb5af9698b
Albert Lionelle <albert.lionelle@colostate.edu>
2024-05-06 12:50:53 -0600

    feat: updated main

--- Module05/Student.java
+++ Module05/Student.java
@@ -1,3 +1,5 @@
+/** feel free to use this template as a starting point. */
+
 public class Student extends Person {
     private int id;
 
@@ -15,11 +17,15 @@ public class Student extends Person {
         Person e1 = new Student("Alice", 1);
         Person e2 = new Student("Bob", 2);
         Person e3 = new Student("Carol", 3);
+        Person e5 = new Person("Alice");
         Person arrayOfPeople[] = {e1, e2, e3};
 
         Person e4 = new Student("Alice", 1); // same as e1?
 
         System.out.println(e1 == arrayOfPeople[0]); // what is printed here?
         System.out.println(e1 == e4); // what is printed here?
+
+        System.out.println(e1.equals(e4)); // and here?
+        System.out.println(e1.equals(e5)); // and here?
     }
 }
