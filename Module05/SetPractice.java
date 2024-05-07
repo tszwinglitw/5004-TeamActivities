fc37316b07e573a21fa8a734ec886758e59291a8
Albert Lionelle <albert.lionelle@colostate.edu>
2024-05-06 21:42:31 -0600

    doc: added TODO comment

--- Module05/SetPractice.java
+++ Module05/SetPractice.java
@@ -22,22 +22,10 @@ public class SetPractice {
         System.out.println(set.contains("Alice"));
 
 
-        Set<Person> people = new HashSet<>();
+        // TODO: add Set<Person>, and see
+        // how it behaves with both students and person.
+        // Make sure to have a duplicate student or person
 
-        Person p1 = new Person("Alice");
-        Person p2 = new Student("Alice", 1);
-        Person p3 = new Student("Alice", 2);
-        Person p4 = new Student("Alice", 1); // same as p2?
-
-        people.add(p1);
-        people.add(p2);
-        people.add(p3);
-        people.add(p4);
-
-        System.out.println(people);
-
-        System.out.println(people.contains(p1));
-        System.out.println(people.contains(p4));
 
 
     }
